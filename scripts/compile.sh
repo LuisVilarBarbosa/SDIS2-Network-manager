9a5b452517901381028f38e6e411e79f150f7bf4
José Carlos Coutinho <jczelik@gmail.com>
2017-05-26 20:19:20 +0100

    Not detecting JDBC

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,7 +2,7 @@
 cd .. || exit
 mkdir -p bin
 echo "Compiling..."
-javac -d bin NetworkManager.java
+javac -classpath ".:sqlite-jdbc-3.16.1.jar" -d bin NetworkManager.java
 cd scripts
 echo "Press any key to continue . . ."
 read -r var
--- scripts/peer1.sh
+++ scripts/peer1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd .. || exit
-java -cp bin NetworkManager new_group 3000 localhost 3000
+java -classpath ".:sqlite-jdbc-3.16.1.jar" NetworkManager new_group 3000 localhost 3000
 cd scripts
 echo "Press any key to continue . . ."
 read -r var
