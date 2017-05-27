2842c8b9e52e3293b6624ba7e6d93b2c0ff7f4c3
José Carlos Coutinho <jczelik@gmail.com>
2017-05-27 16:29:14 +0100

    peer2 changed

--- scripts/peer2.sh
+++ scripts/peer2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd .. || exit
-java -cp "bin:sqlite-jdbc-3.16.1.jar" NetworkManager join_group 3001 localhost 3001 localhost 3000
+java -cp "bin:sqlite-jdbc-3.16.1.jar" NetworkManager join_group 3001 172.30.18.9 3001 172.30.24.141 3000
 cd scripts
 echo "Press any key to continue . . ."
 read -r var
