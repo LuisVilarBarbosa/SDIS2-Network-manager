5fdbe56691d9824fbe1a3b02aae3c66af133e677
José Carlos Coutinho <jczelik@gmail.com>
2017-05-28 18:18:39 +0100

    Change permissions basic version done

--- db/ChangePermissionsPacket.java
+++ db/ChangePermissionsPacket.java
@@ -1,6 +1,12 @@
 package db;
 
-public class ChangePermissionsPacket {
+import java.io.Serializable;
+
+public class ChangePermissionsPacket implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String username;
 	private boolean isAdmin;
 	
